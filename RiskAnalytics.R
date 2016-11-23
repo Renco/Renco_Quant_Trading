4ad24e5057ae5bc108768c718cf052ecbcfb47b0
Renco <rencochy@live.cn>
2016-11-23 13:31:09 -0500

    update risk contribution

--- RiskAnalytics.R
+++ RiskAnalytics.R
@@ -1,17 +1,19 @@
+#function for risk management
 renco.risk.management <- function(){
+
 require(quantmod)
 require(PerformanceAnalytics) #Do VaR
 require(magrittr)
 
-holding.tickers <- c("600419.SS","000613.SZ",
-                     "600506.SS","600333.SS",
-                     "000036.SZ","002673.SZ",
-                     "600272.SS","000833.SZ")
+holding.tickers <- c("000002.SZ"
+                    )
+#get data
 getSymbols(holding.tickers, from = "2015-09-01")
-position <- c(25,400,300,300,400,200,100,1000)
+position <- c(400)
 
 portfolio <- length(position) %>% rep(1,.)
 for (i in 1:length(portfolio)){
+  #calculate the value of position
   portfolio[i] <- position[i] * last(Cl(get(holding.tickers[i])))
 }
 
@@ -29,18 +31,27 @@ s <- VaR(VaRdata, p = 0.8, method = "modified",
          weights = portfolio / sum(portfolio))
 # risk.exposure <- as.vector (-s$contribution * portfolio )
 # print(risk.exposure)
-total.exposure <- sum(portfolio) * (-s$MVaR)
+total.exposure <- data.frame(sum(portfolio) * (-s$MVaR))
+names(total.exposure) <- "Total Exposure"
 print(total.exposure)
 risk.contribution <- as.vector(s$pct_contrib_MVaR*100) 
 names(risk.contribution) <- holding.tickers
 #print(risk.contribution)
+
+###draw a pie graph for risk attribution
 pie(s$pct_contrib_MVaR,
     labels = paste(holding.tickers," ",round(risk.contribution,2),"%"),
     col=rainbow(length(portfolio)))
 
-rbind(round(risk.contribution > 100 * portfolio / sum(portfolio)),
-     risk.contribution - 100 * portfolio / sum(portfolio))
+risk.summary <- rbind(round(risk.contribution > 100 * portfolio / sum(portfolio)),
+     risk.contribution - 100 * portfolio / sum(portfolio)) %>% t() 
+risk.summary <- data.frame(risk.summary)
 
+names(risk.summary) <- c("Risk Contribution","Active Risk")
+
+print(risk.summary)
+#Renco defines active risk as the risk contribution in excess of 
+#portfolio weight
 
 # risk.exposure <- as.vector (s * portfolio )
 # names(risk.exposure) <- holding.tickers
